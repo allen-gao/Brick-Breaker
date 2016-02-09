53755529f50772eee75bbede1513c598223bf60b
Allen Gao <allen001@live.ca>
2016-02-08 21:46:08 -0500

    Project init

---
+++ src/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Brick Breaker");
+		
+		frame.setResizable(false);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		
+	}
+
+}
