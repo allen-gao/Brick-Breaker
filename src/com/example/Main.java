9da98f2c5e7db04c917d19c3217628109d94649c
Allen Gao <allen.gao@uwaterloo.ca>
2016-02-09 00:08:44 -0500

    wip

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,16 +1,34 @@
 package com.example;
 
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Main {
+public class Main extends JComponent {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Brick Breaker");
 		
-		frame.setResizable(false);
-		frame.setSize(500, 500);
-		frame.setVisible(true);
-		
-	}
+		SwingUtilities.invokeLater(new Runnable() {
 
+			public void run() {
+				JFrame frame = new JFrame("Brick Breaker");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
+				
+				JPanel panel = new JPanel();
+				JButton button = new JButton("Ok");
+				panel.add(button);
+				button.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+						System.exit(1);
+					}
+				});
+				
+				frame.add(panel);
+				
+				frame.setResizable(true);
+				frame.setSize(500, 500);
+				frame.setVisible(true);
+			}
+		});
+	}
 }
