07e2298630b1fe8b9df8d779e324bc30dee602fd
Allen Gao <allen.gao@uwaterloo.ca>
2016-02-15 05:54:48 -0500

    implemented machine tilt

--- src/com/example/GameWindow.java
+++ src/com/example/GameWindow.java
@@ -82,6 +82,12 @@ public class GameWindow extends JPanel {
 					}
 					paused = !paused;
 				}
+				if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
+					gameLogic.ball.xSpeed -= 1;
+				}
+				if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+					gameLogic.ball.xSpeed += 1;
+				}
 			}
 		});
 		
@@ -189,11 +195,11 @@ public class GameWindow extends JPanel {
 		drawCenterString(g, "Use the mouse to aim where you want the ball to shoot.", 130, 0);
 		drawCenterString(g, "Once you're ready, simply left click and start playing!", 150, 0);
 		drawCenterString(g, "Break the rainbow brick for a special powerup!", 170, 0);
-		drawCenterString(g, "Press esc to exit back to this menu and P to pause the game.", 210, 0);
+		drawCenterString(g, "Use the left and right arrow keys to 'tilt' the screen.", 190, 0);
+		drawCenterString(g, "Press esc to exit back to this menu and P to pause the game.", 230, 0);
 		
-		drawCenterString(g, "You have 3 lives to try and get the highest score possible.", 230, 0);
-		drawCenterString(g, "Good luck!", 250, 0);
-		
+		drawCenterString(g, "You have 3 lives to try and get the highest score possible.", 250, 0);
+
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 35));
 		drawCenterString(g, "Cheats", 300, 0);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 15));
